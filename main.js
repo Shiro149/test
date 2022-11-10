fc9d7df624205763ea1b34c256f505e71ce3877d
Cường Trần <shiro1492001@gmail.com>
2022-11-10 14:06:39 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+// var a=1
+// var b=2
+// if ( a==b){
+//     console.log('Dieu kien dung!');
+// }else{
+//     console.log('Dieu kien sai!');
+// }
+
+
+// var isSuccess = true;
+
+// console.log(isSuccess);
+
+// var fullname = 0;
+
+//  if (fullname) {
+//     console.log('Dieu kien dung');
+//  } else {
+//     console.log('dieu kien sai');
+//  }
+
+
+
+// var a = 1;
+// var b = 2;
+// var c = 3;
+
+// if (!(a < 0)) {
+//     console.log('Dieu kien dung');
+// }
+
+
+
+// // Number type
+// var a = 1;
+// var b = 2;
+// var c = 1.5;
+
+// //String type
+// var fullName = 'Cuong Tran';
+
+// console.log(fullName);
+
+// // boolean
+// var isSuccess = true;
+
+
+var a = '1';
+var b = 1;
+
+console.log(a !== b);
+
